36ef717b7b3374818c44306bd500f6aef0a706b0
Haseeb Qureshie <hqureshi@f1r6s15.codam.nl>
2022-11-15 10:01:13 +0100

    Setup changes

---
+++ Makefile
@@ -0,0 +1,54 @@
+NAME = cub3d
+
+HEADER = inc/
+
+CFLAGS = -g -I include 
+
+LFLAGS = -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit
+
+CC = gcc -Wall -Werror -Wextra
+
+LIBFT = lib/libft/libft.a
+MLX = lib/mlx/libmlx42.a
+
+OBJS_DIR = objs
+SRC_DIR = src
+
+INC	:=	-I $(INCLUDE_DIR)
+
+SRCS =	main.c
+
+OBJS =	$(addprefix $(OBJS_DIR)/,$(SRCS:.c=.o))
+
+all: $(LIBFT) $(MLX) $(NAME)
+
+$(OBJS_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) $(INC) -I -c -o $@ $<
+
+$(LIBFT):
+	$(MAKE) -C lib/libft
+
+$(MLX):
+	@$(MAKE) -C lib/mlx 
+
+$(NAME): $(LIBFT) $(MLX) $(OBJS)
+	@$(CC) $(CFLAGS) $(INC) -I $^ -o $(NAME) $(LFLAGS)
+	@echo "\033[92mFiles made\033[0m"
+
+clean:
+	@rm -rf $(OBJS_DIR)
+	@make clean -C lib/libft
+	@rm -f libmlx.dylib
+	@echo "\033[1;34mA\033[1;31ml\033[1;32ml \033[1;33mc\033[1;30ml\033[1;35me\033[1;36ma\033[1;37mn\033[0m"
+
+fclean:
+	@rm -rf $(OBJS_DIR)
+	@rm -f $(NAME)
+	@rm -f $(MLX)
+	@make fclean -C lib/libft
+	@echo "\033[0;31mFiles cleaned\033[0m"
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ inc/cub3d.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hqureshi <hqureshi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 09:48:29 by hqureshi          #+#    #+#             */
+/*   Updated: 2022/11/15 09:50:09 by hqureshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/mlx/include/MLX42/MLX42.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <memory.h>
+#define WIDTH 800
+#define HEIGHT 600
